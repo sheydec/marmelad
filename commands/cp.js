6a84b2946f475ce8c9aaca6cba969fb8be4c09ea
Yunus Gaziev <yunus.gaziev@gmail.com>
2018-06-09 14:04:30 +0500

    Исправление для cp create-page

--- commands/cp.js
+++ commands/cp.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
-const path = require('path');
 
 module.exports = (name) => {
-  const pagePath = path.join('marmelad', '_pages', `${name}.html`);
+  const pagePath = `_pages/${name}.html`;
 
   if (fs.existsSync(pagePath)) {
     console.log();
     process.stdout.write(`page ${pagePath} is already exists`);
   } else {
     fs.writeFileSync(pagePath, '', { encoding: 'utf8' });
+    console.log();
     process.stdout.write(`page ${pagePath} is created`);
   }
 
